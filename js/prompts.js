c7372ece54e00f1ddde766399de11f30dcfe1d6b
Wonvy <iamwonvy@qq.com>
2023-09-27 07:11:42 +0000

    Update prompts.js 判断text是否为空

--- js/prompts.js
+++ js/prompts.js
@@ -2,15 +2,24 @@ export { prompts_DeleteCommand, prompts_GetCommand, prompts_splitwords };
 
 // 删除指令
 function prompts_DeleteCommand(text) {
-  return text.replace(/--\w+[^-]*/g, "");
+  if (text === null || text === undefined) {
+    return "";
+  }
+  return text.replace(/--\w+[^-]*/g, ""); // 执行 replace 操作
 }
 
 // 提取指令，返回数组
 function prompts_GetCommand(text) {
+  if (text === null || text === undefined) {
+    return "";
+  }
   return text.match(/--\w+[^-]*/g);
 }
 
 // 拆分描述词
 function prompts_splitwords(text) {
+  if (text === null || text === undefined) {
+    return "";
+  }
   return text.match(/[^,.]+[,.]/g);
 }
